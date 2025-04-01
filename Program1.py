a120b8f669db96bc27c2323aacffc0b9e194566e
Holly <miaw@1234.com>
2025-04-01 14:19:21 -0500

    Actualizó con la creación desde aqui con comando echo blablabla > bla.py

---
+++ Program1.py
@@ -0,0 +1,5 @@
+#Estará compuesto por Entrada Procesamiento y Salida
+2 = print
+
+2("print")
+print(type(2))
--- trabajo.py
+++ trabajo.py
@@ -1,5 +1,58 @@
 ##Holaa Aritmetica basica
-##git add ##git push -u origin main
+##git add #git commit "blablabla" ##git push 
 x = 2
 b = x + 4
 print (b)
+print (float(17) + 23)
+s = 117
+print (s)
+print (type (s))
+l = float (s)
+print (l)
+print (type(l))
+#String
+d="123"
+print (d)
+print (type (d))
+f = int(d)
+print (type(f))
+print (f + 545)
+
+#Input
+
+Nam = input ("¿Quién eres? ")
+
+print ("Bienvenido," + (Nam))
+
+x = 1 + 2 * 3 - 8 / 4
+print (x)
+
+
+#Condicional
+
+x = input ("Ingrese un número: ")
+Y = int(x)
+
+if Y <10:
+    print("pequeño")
+if Y > 20: 
+    print("Grandote")
+
+print("Fin de la cita.")
+
+#Comparasion Operators 
+
+x = (int(input("Escriba un número de 1 a 10: ")))
+print (type(x))
+if x == 5 :
+    print ("Igual a 5")
+if x > 4:
+    print ("Mayor que 4")
+if x >= 5:
+    print ("Es igual o mayor a 5")
+if x < 6 :
+    print ("Es menor que 6")
+if x <= 5: 
+    print ("Es igual o menor a 5")
+if x != 6:
+    print ("No es igual a 6")
